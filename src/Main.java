212554fbc901c57d6e9e76b6e5b8f84f0c585c08
Aksaev <mail-ax@ya.ru>
2022-10-03 19:00:38 +0400

    ДЗ Выполнил.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
+    public static void printSeparator() {
+        System.out.println("--------------------------------------------");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Домашнее задание. Урок 1.10 Методы объектов.");
+        printSeparator();
+
+        Author author1 = new Author("Михаил", "Лермонтов");
+        Book book1 = new Book("Родина", author1, 1841);
+        System.out.println(book1);
+        printSeparator();
+
+        Author author2 = new Author("Александр", "Пушкин");
+        Book book2 = new Book("Борис Годунов", author2, 1825);
+        System.out.println(book2);
+        printSeparator();
+
+        book2.setYearPublication(2022);
+        System.out.println(book2);
+        printSeparator();
     }
 }
